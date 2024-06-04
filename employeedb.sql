86cbd12ada44d8dab46863aea887ef09f3538cdc
markkybean <marksantos320@gmail.com>
2024-06-04 19:29:48 +0800

    added db

---
+++ employeedb.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 04, 2024 at 01:26 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `employeedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employeefile`
+--
+
+CREATE TABLE `employeefile` (
+  `recid` bigint(20) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `age` int(11) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `civilstat` varchar(20) NOT NULL,
+  `contactnum` varchar(20) NOT NULL,
+  `salary` decimal(10,2) NOT NULL,
+  `isactive` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employeefile`
+--
+
+INSERT INTO `employeefile` (`recid`, `fullname`, `address`, `birthdate`, `age`, `gender`, `civilstat`, `contactnum`, `salary`, `isactive`) VALUES
+(1, 'Mark Santos', 'Amaya, Tanza Cavite', '2000-05-10', 24, 'Male', 'Single', '09381394091', 19000.00, 1),
+(2, 'Test', 'Pilipinas', '0000-00-00', 25, 'Male', 'Single', '2345325234', 20000.00, 1),
+(3, 'Testing', 'Aurora', '2002-02-02', 22, 'Male', 'Single', '09381394435', 30000.00, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'marksantos', 'marksantos320@gmail.com', '$2y$10$TjarYMBgYMAl4tzEBgZKpORK9j/aMc1ZvlzSV6uzCl/V3vx46Kq6q'),
+(2, 'Test', 'test@gmail.com', '$2y$10$zfQvJigtWgovBW.QFE2V5uy1ipzCj/fDwbpmODAFgNV9T9cOLx3VC');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employeefile`
+--
+ALTER TABLE `employeefile`
+  ADD PRIMARY KEY (`recid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employeefile`
+--
+ALTER TABLE `employeefile`
+  MODIFY `recid` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
