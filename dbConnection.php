343fad6d65b5ce7ddcb931d988a92c213f80ab2b
markkybean <marksantos320@gmail.com>
2024-06-24 11:46:54 +0800

    test

--- dbConnection.php
+++ dbConnection.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'employeeajaxdb';
+$database = 'employeedb';
 
 // Create a connection (outside any functions)
 $conn = new mysqli($host, $username, $password, $database);
